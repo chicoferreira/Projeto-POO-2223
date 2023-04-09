a291063f5912264a8ba4aac3b7db1f428f37ca04
Júlio Pinto <pinto.julioj03@gmail.com>
2023-04-09 21:34:45 +0100

    fix: entity not found exception

--- src/main/java/com/marketplace/vintage/model/EntityNotFoundException.java
+++ src/main/java/com/marketplace/vintage/model/EntityNotFoundException.java
@@ -3,11 +3,7 @@ package com.marketplace.vintage.model;
 import javax.swing.text.html.parser.Entity;
 
 public class EntityNotFoundException extends Exception {
-    private Class<?> entity;
-
-    public EntityNotFoundException(ParcelCarrier carrierTaken, String exception) throws Exception {
-
-        throw new Exception(exception);
-
+    public EntityNotFoundException(String exception) throws Exception {
+        throw new Exception("Entity not found:" + exception);
     }
 }
