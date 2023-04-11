eb8b98115bb220bed254f81dcad4d739959c02e2
chico ferreira <36338391+chicoferreira@users.noreply.github.com>
2023-04-11 10:08:11 +0100

    feat: Mala getPriceCorrection implementation

---
+++ src/test/java/com/marketplace/vintage/model/impl/MalaItemTest.java
@@ -0,0 +1,22 @@
+package com.marketplace.vintage.model.impl;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MalaItemTest {
+
+    @Test
+    void getPriceCorrection() {
+        BigDecimal basePrice = BigDecimal.valueOf(100);
+        int appreciationRateOverYears = 10;
+
+        MalaItem malaItem = new MalaItem(null, null, null, null, basePrice, null, 0, null, 2022, appreciationRateOverYears);
+
+        assertEquals(basePrice, malaItem.getPriceCorrection(2022));
+        assertEquals(BigDecimal.valueOf(110.0).compareTo(malaItem.getPriceCorrection(2023)), 0);
+        assertEquals(BigDecimal.valueOf(121.0).compareTo(malaItem.getPriceCorrection(2024)), 0);
+    }
+}
