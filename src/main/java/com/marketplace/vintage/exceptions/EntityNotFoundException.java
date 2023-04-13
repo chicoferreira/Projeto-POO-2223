1c7e4c6bfcf1f72261c1ebcb00a451de70fd0adc
chico ferreira <36338391+chicoferreira@users.noreply.github.com>
2023-04-13 15:42:02 +0100

    feat: Unit test for ParcelCarrierManager

--- src/main/java/com/marketplace/vintage/exceptions/EntityNotFoundException.java
+++ src/main/java/com/marketplace/vintage/exceptions/EntityNotFoundException.java
@@ -1,9 +1,8 @@
 package com.marketplace.vintage.exceptions;
 
-import java.lang.RuntimeException;
-
 public class EntityNotFoundException extends RuntimeException {
+
     public EntityNotFoundException(String exception) {
-        super("Entity not found:" + exception);
+        super("Entity not found: " + exception);
     }
 }
