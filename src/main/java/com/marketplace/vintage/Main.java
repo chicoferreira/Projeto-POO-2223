9b4f6850d41f406d3e1886881ec1a31c861108be
chicoferreira <36338391+chicoferreira@users.noreply.github.com>
2023-04-14 01:28:35 +0100

    feat: Application logic

--- src/main/java/com/marketplace/vintage/Main.java
+++ src/main/java/com/marketplace/vintage/Main.java
@@ -1,9 +1,8 @@
 package com.marketplace.vintage;
 
-import com.marketplace.vintage.item.condition.UsedItemCondition;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new UsedItemCondition(10, 100).getDisplayString());
+        VintageApplication vintageApplication = new VintageApplication();
+        vintageApplication.start();
     }
 }
---
+++ src/main/java/com/marketplace/vintage/VintageApplication.java
@@ -0,0 +1,60 @@
+package com.marketplace.vintage;
+
+import com.marketplace.vintage.logging.JavaLogger;
+import com.marketplace.vintage.logging.Logger;
+import com.marketplace.vintage.terminal.Terminal;
+import com.marketplace.vintage.view.View;
+import com.marketplace.vintage.view.ViewFactory;
+import com.marketplace.vintage.view.ViewType;
+
+import java.util.stream.Collectors;
+
+public class VintageApplication {
+
+    private final Terminal terminal;
+    private final Logger logger;
+    private final ViewFactory viewFactory;
+
+    public VintageApplication() {
+        this.viewFactory = new ViewFactory();
+        this.logger = new JavaLogger();
+        this.terminal = new Terminal(this.logger);
+    }
+
+    public void start() {
+        logger.info("Welcome to the Vintage Marketplace!");
+        logger.info();
+
+        String availableViews = buildAllViewsString() + " or Exit (exit)";
+
+        while (true) {
+            logger.info("Choose which view you want to use.");
+            logger.info("Available views: " + availableViews);
+
+            View view = null;
+            while (view == null) {
+                String viewTypeName = terminal.askForInput();
+                if (viewTypeName.equals("exit")) {
+                    return;
+                }
+                ViewType viewType = ViewType.fromCommandName(viewTypeName);
+
+                if (viewType == null) {
+                    logger.info("Invalid view name.");
+                    logger.info("Please type one of the following: " + availableViews);
+                    continue;
+                }
+                view = viewFactory.createView(viewType);
+            }
+
+            view.run();
+        }
+    }
+
+    public String buildAllViewsString() {
+        return ViewType.getAllViewTypes().stream()
+                       .map(viewType -> viewType.getDisplayName() + " (" + viewType.getCommandName() + ")")
+                       .collect(Collectors.joining(", "));
+    }
+
+}
