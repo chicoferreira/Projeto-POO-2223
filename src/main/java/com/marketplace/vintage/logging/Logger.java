b663dfb08cf5f96f8f41b32b0071ae27c70544b5
chicoferreira <36338391+chicoferreira@users.noreply.github.com>
2023-04-14 01:24:12 +0100

    feat: Simple logger implementation

--- src/main/java/com/marketplace/vintage/logging/Logger.java
+++ src/main/java/com/marketplace/vintage/logging/Logger.java
@@ -2,6 +2,10 @@ package com.marketplace.vintage.logging;
 
 public interface Logger {
 
+    default void info() {
+        info("");
+    }
+
     void info(String message);
 
     void warn(String message);
