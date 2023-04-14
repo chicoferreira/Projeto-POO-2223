0f2fd63cc9d5ff1bc73ee173205482264cd11ec4
Rui Lopes <76881129+RuiL1904@users.noreply.github.com>
2023-04-15 00:11:11 +0100

    fix: Avoid NPE on isValidEmail method

--- src/main/java/com/marketplace/vintage/utils/EmailUtils.java
+++ src/main/java/com/marketplace/vintage/utils/EmailUtils.java
@@ -8,6 +8,10 @@ public class EmailUtils {
             Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
     public static boolean isValidEmail(String emailStr) {
+        if (emailStr == null) {
+            return false;
+        }
+
         Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailStr);
         return matcher.find();
     }
