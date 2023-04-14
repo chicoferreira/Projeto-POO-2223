e2d1c8e23d23678eb966e83ed636cecabd161dc8
chicoferreira <36338391+chicoferreira@users.noreply.github.com>
2023-04-14 01:28:07 +0100

    feat: View to abstract multiple views for the application

---
+++ src/main/java/com/marketplace/vintage/view/ViewType.java
@@ -0,0 +1,43 @@
+package com.marketplace.vintage.view;
+
+import com.marketplace.vintage.Lang;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public enum ViewType {
+
+    USER(Lang.VIEW_TYPE_USER_DISPLAY_NAME.get()), CARRIER(Lang.VIEW_TYPE_CARRIER_DISPLAY_NAME.get());
+
+    private static final Map<String, ViewType> VIEW_TYPE_MAP;
+
+    static {
+        VIEW_TYPE_MAP = new HashMap<>();
+        for (ViewType viewType : ViewType.values()) {
+            VIEW_TYPE_MAP.put(viewType.getCommandName(), viewType);
+        }
+    }
+
+    private final String displayName;
+
+    ViewType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public static List<ViewType> getAllViewTypes() {
+        return List.of(ViewType.values());
+    }
+
+    public static ViewType fromCommandName(String commandName) {
+        return VIEW_TYPE_MAP.get(commandName);
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public String getCommandName() {
+        return name().toLowerCase();
+    }
+}
