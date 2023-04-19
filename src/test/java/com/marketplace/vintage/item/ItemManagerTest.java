80eb5de3fbe39625d9cac835cfb72b041e70e128
Júlio Pinto <pinto.julioj03@gmail.com>
2023-04-19 20:13:08 +0100

    refactor: item manager tests

--- src/test/java/com/marketplace/vintage/item/ItemManagerTest.java
+++ src/test/java/com/marketplace/vintage/item/ItemManagerTest.java
@@ -17,7 +17,19 @@ public class ItemManagerTest {
     @Test
     void testItemManager() {
         ItemManager itemManager = new ItemManager();
+        Item testItem = createTestItem();
+        UUID testItemID = testItem.getItemUuid();
 
+        itemManager.addItem(testItem);
+        Item testGetItem = itemManager.getItem(testItemID);
+
+        assertEquals(testItemID, testGetItem.getItemUuid());
+
+        assertThrowsExactly(EntityNotFoundException.class, () -> itemManager.getItem(UUID.randomUUID()));
+        assertThrowsExactly(EntityAlreadyExistsException.class, () -> itemManager.addItem(testGetItem));
+    }
+
+    private Item createTestItem() {
         ItemCondition testCondition = NEW;
         String testDescription = "TEST";
         String testBrand = "BRAND";
@@ -25,34 +37,12 @@ public class ItemManagerTest {
         BigDecimal testBasePrice = BigDecimal.valueOf(100);
         UUID testCarrier = UUID.randomUUID();
 
-        Item testItem = new Item(testCondition, testDescription, testBrand, testNumericCode, testBasePrice, testCarrier) {
+        return new Item(testCondition, testDescription, testBrand, testNumericCode, testBasePrice, testCarrier) {
             @Override
             public BigDecimal getPriceCorrection(int currentYear) {
                 return new BigDecimal(0);
             }
         };
-
-        UUID testItemID = testItem.getItemUuid();
-
-        itemManager.addItem(testItem);
-        Item testGetItem = itemManager.getItem(testItemID);
-
-        assertEquals(testCondition, testGetItem.getItemCondition());
-        assertEquals(testDescription, testGetItem.getDescription());
-        assertEquals(testBrand, testGetItem.getBrand());
-        assertEquals(testNumericCode, testGetItem.getAlphanumericCode());
-        assertEquals(testBasePrice, testGetItem.getBasePrice());
-        assertEquals(testCarrier, testGetItem.getParcelCarrierUuid());
-
-        Item clonedItem = new Item(testItemID, testCondition, testDescription, testBrand, testNumericCode, testBasePrice, testCarrier) {
-            @Override
-            public BigDecimal getPriceCorrection(int currentYear) {
-                return new BigDecimal(0);
-            }
-        };
-
-        assertThrowsExactly(EntityNotFoundException.class, () -> itemManager.getItem(UUID.randomUUID()));
-        assertThrowsExactly(EntityAlreadyExistsException.class, () -> itemManager.addItem(clonedItem));
-
     }
 }
+
