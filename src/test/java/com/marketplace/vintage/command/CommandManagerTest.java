eed73fd62b0a60a8e887e8ed3eb6c9e8b2df37a0
chico ferreira <36338391+chicoferreira@users.noreply.github.com>
2023-04-22 16:38:47 +0100

    feat: Add check for min args when executing command

--- src/test/java/com/marketplace/vintage/command/CommandManagerTest.java
+++ src/test/java/com/marketplace/vintage/command/CommandManagerTest.java
@@ -4,6 +4,7 @@ import com.marketplace.vintage.logging.Logger;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 class CommandManagerTest {
 
@@ -12,7 +13,7 @@ class CommandManagerTest {
         CommandManager commandRepository = new CommandManager();
         commandRepository.registerCommand(new BaseCommand("test", "", 0, "Test command") {
             @Override
-            public void execute(Logger logger, String[] args) {
+            public void executeSafely(Logger logger, String[] args) {
                 Assertions.assertEquals(2, args.length);
                 Assertions.assertEquals("arg1", args[0]);
                 Assertions.assertEquals("arg2", args[1]);
@@ -22,5 +23,22 @@ class CommandManagerTest {
         commandRepository.executeRawCommand(null, "test arg1 arg2");
         commandRepository.executeRawCommand(null, "test arg1 arg2  ");
         commandRepository.executeRawCommand(null, "test   arg1  arg2  ");
+    }
+
+    @Test
+    void executeCommandWithNotEnoughArgs() {
+        Logger logger = Mockito.mock(Logger.class);
+
+        CommandManager commandRepository = new CommandManager();
+        commandRepository.registerCommand(new BaseCommand("test", "test <arg1> <arg2>", 2, "Test command") {
+            @Override
+            public void executeSafely(Logger logger, String[] args) {
+                Assertions.fail("Should not be called");
+            }
+        });
+
+        commandRepository.executeRawCommand(logger, "test");
+        commandRepository.executeRawCommand(logger, "test arg1");
+        Mockito.verify(logger, Mockito.times(2)).warn("Usage: test <arg1> <arg2>");
     }
 }
