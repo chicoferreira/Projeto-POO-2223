db4088af6460a346f6c698530d7c9d31a32c8811
chicoferreira <36338391+chicoferreira@users.noreply.github.com>
2023-04-27 03:03:01 +0100

    feat: Add logout command to exit the current view

---
+++ src/main/java/com/marketplace/vintage/commands/LogoutCommand.java
@@ -0,0 +1,22 @@
+package com.marketplace.vintage.commands;
+
+import com.marketplace.vintage.command.BaseCommand;
+import com.marketplace.vintage.logging.Logger;
+import com.marketplace.vintage.view.BaseView;
+
+public class LogoutCommand extends BaseCommand {
+
+    private final BaseView targetView;
+
+    public LogoutCommand(BaseView targetView) {
+        super("logout", "logout", 0, "Logs out of the current view");
+        this.targetView = targetView;
+    }
+
+    @Override
+    protected void executeSafely(Logger logger, String[] args) {
+        this.targetView.setShouldExit();
+        logger.info("Logging out...");
+        logger.info();
+    }
+}
