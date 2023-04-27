00c9fd036dea6063ac37ffe81a53c9773b4a9b0c
chicoferreira <36338391+chicoferreira@users.noreply.github.com>
2023-04-27 03:08:49 +0100

    refactor: Simplified ItemManager test

--- src/main/java/com/marketplace/vintage/utils/StringUtils.java
+++ src/main/java/com/marketplace/vintage/utils/StringUtils.java
@@ -1,11 +1,11 @@
 package com.marketplace.vintage.utils;
 
-import java.util.List;
+import java.util.Collection;
 import java.util.stream.Collectors;
 
 public class StringUtils {
 
-    public static String joinQuoted(List<String> strings, String delimiter) {
+    public static String joinQuoted(Collection<String> strings, String delimiter) {
         return strings.stream()
                       .map(s -> "'" + s + "'")
                       .collect(Collectors.joining(delimiter));
