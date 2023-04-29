e80ca1a0df2b78ef02b59e89539d867c9fdf8127
chicoferreira <36338391+chicoferreira@users.noreply.github.com>
2023-04-29 17:46:15 +0100

    refactor: Change number format

--- src/main/java/com/marketplace/vintage/VintageConstants.java
+++ src/main/java/com/marketplace/vintage/VintageConstants.java
@@ -6,6 +6,6 @@ public class VintageConstants {
 
     public static final String DEFAULT_EXPEDITION_PRICE_EXPRESSION_STRING = "basePrice * 0.1 * (1 + tax) * 0.9";
     public static final List<String> DEFAULT_EXPEDITION_PRICE_EXPRESSION_VARIABLES = List.of("basePrice", "tax");
-    public static final String DISPLAY_ITEM_FORMAT = "[<id>] (<itemType>) <description> (of brand '<brand>') - <finalPrice>U.M. (sent by <parcelCarrier>)";
+    public static final String DISPLAY_ITEM_FORMAT = "[<id>] (<itemType>) <description> (of brand '<brand>') - <finalPrice> (sent by <parcelCarrier>)";
 
 }
